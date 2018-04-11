3f231411e4207e3cd1c18c84ab054b69b106f01c
Fabien Moritz <fabien.moritz@gmail.com>
2018-04-11 09:45:25 +0200

    remplace http-server par express (désolé c'est pas mon tour...)

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/yo', function (req, res) {
+    res.send('Hello World!')
+});
+
+
+app.listen(8080, function () {
+    console.log('Example app listening on port 3000!')
+});
